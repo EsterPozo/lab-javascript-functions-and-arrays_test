9c7794a526f1f40886468765ab54dc1bb71b7379
EsterPozo <pozo.estherr@gmail.com>
2022-11-19 11:40:52 +0100

    added: 3.1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,24 +1,66 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num2 > num1) {
+    return num2;
+  } else if (num1 === num2) {
+    return num1;
+  }
+}
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
-
-
+function findLongestWord(someArr) {
+  let i = 0;
+  let longest = "";
+  while ( i < someArr.length) {
+    if (someArr[i].length > someArr[i+1].length) {
+  longest = someArr[i];
+  } else if (someArr[i].length < someArr[i+1].length) {
+  longest = someArr[i+1];
+  } else if (someArr[i].length === someArr[i+1].length) {
+  longest = someArr[i];
+  }
+  i++;
+  }
+  return longest;
+}
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr) {
+  let sum = 0;
+  for (let i = 0; i<arr.length;i++) {
+ sum += arr[i];
+  }
+  return sum;
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(arr) {
+let sum = 0;
+  for (let i = 0; i<arr.length; i++) {
+    if (typeof arr[i] === string) {
+      sum += arr[i].length
+    } else if (typeof arr[i] === number) {
+      sum += arr[i];
+    } else if (typeof arr[i] === boolean) {
+      
+      if (arr[i]) {
+        let word === "true"
+        sum += word.length;
+      } else {
+        let word === "false";
+        sum += word.length;
+      }
+    } 
+  }
+}
 
 
 
